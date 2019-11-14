fed97cce0f558de27099863805129ef430c4b363
Annihilater <yanmiexingkong@gmail.com>
2019-11-14 11:52:42 +0800

    chore:update author and email

--- 华容道/huarongdao.py
+++ 华容道/huarongdao.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date  : 2019/11/13 20:49
-# @Author: PythonVampire
-# @email : vampire@ivamp.cn
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
 # @File  : huarongdao.py
 import time
 
