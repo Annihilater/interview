93db83202ef4094c9aee961705eeeec38113bed4
Annihilater <yanmiexingkong@gmail.com>
2019-12-18 18:25:44 +0800

    refactor: use insert_sort verify other_sort algorithm

--- 八大排序算法/排序八:基数排序/radix_sort.py
+++ 八大排序算法/排序八:基数排序/radix_sort.py
@@ -8,6 +8,8 @@ import copy
 import random
 from typing import List
 
+from 八大排序算法.lib.verify import verify
+
 
 def radix_sort(l: List) -> List:
     """
@@ -39,47 +41,5 @@ def radix_sort(l: List) -> List:
     return l
 
 
-def bubble_sort(l: List) -> List:
-    """
-    冒泡排序（升序）
-    :param l:源数组
-    :return:排序后的数组
-    """
-    if len(l) == 0 or len(l) == 1:
-        return l
-
-    for i in range(len(l) - 1):
-        changed = False
-        for j in range(len(l) - 1 - i):
-            if l[j] > l[j + 1]:
-                l[j], l[j + 1] = l[j + 1], l[j]
-                changed = True
-        if not changed:  # 如果遍历一次，没有发生调换，则说明数组是有序的
-            return l
-    return l
-
-
-def gen(m: int) -> List:
-    p = []
-    for i in range(10):
-        p.append(random.randint(0, m))
-    return p
-
-
-def main():
-    k = gen(1000)
-    print(f'start: {k}')
-    result = radix_sort(k)
-    print(f'result: {result}')
-    result1 = bubble_sort(copy.deepcopy(k))
-    print(f'result1: {result1}')
-    if result1 != result:
-        print('错误')
-        raise
-    else:
-        print('正确')
-
-
 if __name__ == '__main__':
-    for i in range(1000):
-        main()
+    verify(radix_sort)
